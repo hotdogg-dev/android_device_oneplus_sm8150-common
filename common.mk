f36a880d126ec5545bc9431892e7b81579f85e23
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-01-25 23:13:18 +0100

    sm8150-common: Build goodix lib from source
    * requires https://review.lineageos.org/c/LineageOS/android_hardware_oneplus/+/318767
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- common.mk
+++ common.mk
@@ -247,7 +247,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.oneplus
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor:64
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
