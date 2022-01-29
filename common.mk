016030e082f93f21636afc3bbb9fa8783b8343ea
DarkJoker360 <simoespo159@gmail.com>
2022-01-29 09:50:05 +0100

    sm8150-common: Build authsecret hal
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- common.mk
+++ common.mk
@@ -130,6 +130,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Authsecret
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0.vendor
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl:64 \
