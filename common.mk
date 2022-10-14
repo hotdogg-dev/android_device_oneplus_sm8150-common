f51f6044dd2501952edb8dd91adeb9ff9b7a1794
Robert Shih <robertshih@google.com>
2022-10-14 09:04:25 +0200

    sm8150-common: Enable AIDL Clearkey HAL
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- common.mk
+++ common.mk
@@ -227,7 +227,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 # fastbootd
