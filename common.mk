b5f2770513c3415c2ee24c9483172474a9d0cb0c
DarkJoker360 <simoespo159@gmail.com>
2023-02-13 19:29:49 +0100

    sm8150-common: Enable blur
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- common.mk
+++ common.mk
@@ -159,6 +159,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
