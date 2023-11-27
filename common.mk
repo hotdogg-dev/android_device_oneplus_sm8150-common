bd94f1932f3cbc81a4ef9f7dd66aa869d1ad82f1
DarkJoker360 <simoespo159@gmail.com>
2023-11-27 09:52:53 +0100

    sm8150-common: Drop ui blur
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- common.mk
+++ common.mk
@@ -159,9 +159,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-# Blur
-TARGET_ENABLE_BLUR := true
-
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
